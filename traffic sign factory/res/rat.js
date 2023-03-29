0a534379a079a167ab4ae8d1e91735f8fa8841ed
acf04 <124190846+acf04@users.noreply.github.com>
2023-03-29 10:17:26 -0500

    traffic sign factory-ready

---
+++ traffic sign factory/res/rat.js
@@ -0,0 +1,18 @@
+var rats =
+[
+{
+  'Better':       ['Better','Worser','Greater','Smaller'],
+  'be':           ['be','not to be','having fun'],
+  'a Samurai':    ['a Samurai', 'no one', 'a Gardener', 'yourself'],
+  'in a Garden':  ['in a Garden','in the Heaven','nowhere','at War'],
+  'than':         ['than'],
+  'a Gardener':   ['a Gardener', 'a Samurai', 'a cook', 'a painter'],
+  'in the War':   ['in the War', 'in the Heaven', 'in the store' , 'nowhere']
+},
+{
+  'You':       ['You','I','Grandma','Curiosity'],
+  'killed':    ['killed','revived','painted','made to purr'],
+  'a Rat':     ['a Rat','a Samurai', 'the Cat', 'drivers', 'someone'],
+}
+
+];
